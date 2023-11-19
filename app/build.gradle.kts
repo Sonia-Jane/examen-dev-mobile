77386886492e75eb112fd6cabb590b9a1a4bb257
giressesama237 <gsiledje@gmail.com>
2023-11-19 12:29:39 +0100

    mise a jour du sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.bytequiz"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.bytequiz"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
