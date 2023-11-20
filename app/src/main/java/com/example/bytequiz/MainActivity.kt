8950ece8390b047aabf61ade58e02f731a4174f4
Sonia-Jane <57155173+Sonia-Jane@users.noreply.github.com>
2023-11-20 16:24:57 +0100

    commit

--- app/src/main/java/com/example/bytequiz/MainActivity.kt
+++ app/src/main/java/com/example/bytequiz/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.bytequiz
 
+import android.animation.ObjectAnimator
+import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val progressBar: ProgressBar = findViewById(R.id.progressBar)
+        val color = ContextCompat.getColor(this, R.color.violet)
+        val colorStateList = ColorStateList.valueOf(color as Int)
+        progressBar.setProgressTintList(colorStateList)
+        progressBar.max = 500
+        val currentProgress = 600
+
+        ObjectAnimator.ofInt(progressBar, "progress", currentProgress)
+            .setDuration(2000)
+            .start()
+
     }
+}
+
+class ContextCompat {
+    companion object {
+        fun getColor(mainActivity: MainActivity, customProgressColor: Any) {
+
+        }
+    }
+
 }
